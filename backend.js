508321fade094f1e3fe968def7167380007f5d7d
samukele-dev <samukele107@gmail.com>
2023-07-14 11:42:12 +0200

    a

--- backend.js
+++ backend.js
@@ -1,65 +1,72 @@
 // Import necessary modules and set up Express app
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Pool } = require('pg');
-const fs = require('fs');
-const path = require('path');
+const axios = require('axios');
+const { v4: uuidv4 } = require('uuid');
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Create a new PostgreSQL connection pool
-const pool = new Pool({
-    user: 'your_username',
-    host: 'your_host',
-    database: 'your_database',
-    password: 'your_password',
-    port: 5432, // Default PostgreSQL port
+// Configure Mailchimp API key and list ID
+mailchimp.setConfig({
+    apiKey: '9e98e889029e03a3ee5bbb5c4c3ec441-us21',
+    server: 'us21',
 });
-
-// Create the subscribers table if it doesn't exist
-pool.query(
-        `CREATE TABLE IF NOT EXISTS subscribers (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL
-  );`
-    )
-    .then(() => {
-        console.log('Subscribers table created or already exists');
-    })
-    .catch((error) => {
-        console.error('Error creating subscribers table:', error);
-    });
 
 // Serve the HTML template
 app.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, 'templates', 'subscribe.html'), 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading template file:', err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.send(data);
-        }
-    });
+    res.sendFile(__dirname + '/templates/subscribe.html');
 });
 
 // Handle form submission to subscribe
-app.post('/subscribe', (req, res) => {
+app.post('/subscribe', async(req, res) => {
     const { email } = req.body;
 
-    // Save the email to the database
-    const query = 'INSERT INTO subscribers (email) VALUES ($1)';
-    const values = [email];
+    try {
+        // Check if the email is already subscribed
+        const response = await mailchimp.lists.getListMember('221f70b044', md5(email));
+        if (response.status === 200 && response.data.status === 'subscribed') {
+            res.send('Email is already subscribed');
+            return;
+        }
+    } catch (error) {
+        console.error('Error checking subscriber status:', error);
+    }
 
-    pool.query(query, values)
-        .then(() => {
-            console.log('Email saved to the database');
-            res.send('Subscription successful!');
-        })
-        .catch((error) => {
-            console.error('Error saving email to the database:', error);
-            res.status(500).send('Error saving email to the database');
+    try {
+        // Add the email to the Mailchimp list
+        const subscriber = await mailchimp.lists.addListMember('221f70b044.', {
+            email_address: email,
+            status: 'pending',
+            merge_fields: {
+                // Add any additional merge fields as needed
+            },
         });
+
+        // Generate an activation token
+        const activationToken = uuidv4();
+
+        // Send the activation email to the subscriber
+        const activationLink = `http://your-website.com/activate/${activationToken}`;
+        const mailchimpResponse = await mailchimp.messages.send({
+            message: {
+                subject: 'Activate Your Subscription',
+                html: `<p>Click the following link to activate your subscription: <a href="${activationLink}">${activationLink}</a></p>`,
+                from_email: 'sender@example.com',
+                to: [{ email: email }],
+            },
+        });
+
+        console.log('Email sent to Mailchimp:', mailchimpResponse);
+
+        // You can store the activation token and subscriber details in your database for further processing
+
+        res.send('Activation email sent!');
+    } catch (error) {
+        console.error('Error subscribing and sending activation email:', error);
+        res.status(500).send('Error subscribing and sending activation email');
+    }
 });
 
 // Start the server
